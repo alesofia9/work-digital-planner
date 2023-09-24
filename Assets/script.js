b2a33ee81e11824524186491619fec6072b41571
alesofia9 <alesofia09@gmail.com>
2023-09-24 18:49:02 -0500

    JS working to show current date, edited HTML to enlist JS hour color functions/conditions.

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,54 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+var saveBtn = $('.saveBtn');
+// Retrieves and displays current date.//
+$('#currentDay').text(moment().format('dddd MMMM Do, YYYY'));
+
+function hourColor() {
+  var Hour = moment().hours();
+
+  $('.time-block').each(function() {
+    var nowHour = parseInt($(this).attr('id'));
+  console.log(this); 
+
+    if (nowHour === Hour) {
+      $(this).addClass('present');
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+    } else if (nowHour > Hour) {
+      $(this).addClass('future');
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+    } else {
+      $(this).addClass('past');
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+    }
+  })
+};
+
+saveBtn.on('click', function() {
+  var time = $(this).siblings('.hour').text();
+  var event = $(this).siblings('.description').val();
+
+  localStorage.setItem(time,event);
+});
+
+function usePlanner() {
+  $('.hour').each(function() {
+    var nowHour = $(this).text();
+    var nowPlan = localStorage.getItem(nowHour);
+
+    if (nowHour !== null) {
+      $(this).siblings('.description').val(nowPlan);
+    }
+  });
+}
+
+hourColor();
+usePlanner();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +67,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
