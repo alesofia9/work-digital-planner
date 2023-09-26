92c7e20b613f042e00a9cdbfb00262dce51c2c5e
alesofia9 <alesofia09@gmail.com>
2023-09-25 23:15:56 -0500

    Finalized HTML, CSS, JS, & ReadMe files with added comments, color scheme work flow, user instructions, and js verification.

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 var saveBtn = $('.saveBtn');
 // Retrieves and displays current date.//
 $('#currentDay').text(moment().format('dddd MMMM Do, YYYY'));
 
+// Condition for past, present, and future css colors below. Lines 10 - 11 establishes the hours. Function with variables created to get the time data to reflect color scheme according to the hour.
 function hourColor() {
   var Hour = moment().hours();
 
@@ -29,6 +26,7 @@ function hourColor() {
   })
 };
 
+// Save button activated to store text/user events while storing said events after user clicks on the save button.  
 saveBtn.on('click', function() {
   var time = $(this).siblings('.hour').text();
   var event = $(this).siblings('.description').val();
@@ -36,34 +34,17 @@ saveBtn.on('click', function() {
   localStorage.setItem(time,event);
 });
 
-function usePlanner() {
+// This part was the trickiest. The function below saves a plan/event the user creates by the hour when the user clicks the save button (see saveBtn function). If the user leaves the page but comes back later, the saved event should still populate!  
+function utilizePlanner() {
   $('.hour').each(function() {
     var nowHour = $(this).text();
-    var nowPlan = localStorage.getItem(nowHour);
+    var savePlan = localStorage.getItem(nowHour);
 
     if (nowHour !== null) {
-      $(this).siblings('.description').val(nowPlan);
+      $(this).siblings('.description').val(savePlan);
     }
   });
 }
 
 hourColor();
-usePlanner();
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+utilizePlanner();
